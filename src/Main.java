d40fd9600bab96479914c428ebbd010529fc93aa
AndreiSergeichuk <kyrtman123@gmail.com>
2018-04-29 18:29:11 +0300

    555555

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("commit anton");
         System.out.println("commit anton 2");
         System.out.println("12323");
+        System.out.println("55555555555");
     }
 }
